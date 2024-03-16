714b27c6e0a2dd5b4109f5d2e06ad3c950e63053
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-03-16 13:34:16 -0400

    make JS "more scripty"

--- main/Sisyphus.js
+++ main/Sisyphus.js
@@ -1,22 +1,24 @@
 const NAME = 'Sisyphus'
-
-// should this be a classic `function`?
-const main = () => {
-	let chars = ''
-	// begin eternal torture,
-	// and abuse type-coercion
-	while (Infinity) {
-		// build the string while printing it
-		while (chars.length < NAME.length) {
-			console.log(chars)
-			chars += NAME[chars.length]
-		}
-		// destroy all of our work, char by char
-		while (chars.length > 0) {
-			console.log(chars)
-			// shoult it be `substring` or `substr`?
-			chars = chars.slice(0, -1)
-		}
+var chars = ''
+// begin eternal torture,
+// and abuse type-coercion
+while (Infinity) {
+	// build the string while printing it
+	while (chars.length < NAME.length) {
+		console.log(chars)
+		chars += NAME[chars.length]
+	}
+	// destroy all of our work, char by char
+	while (chars.length > 0) {
+		console.log(chars)
+		/*
+		`substr` is deprecated.
+		`substring` is specific to strings.
+  		`slice` is polymorphic (it also works on `Array`s),
+		so we can use its "duck-typing".
+  		`slice` is short, both in name and args,
+		which is desirable for minification.
+		*/
+		chars = chars.slice(0, -1)
 	}
 }
-main()
