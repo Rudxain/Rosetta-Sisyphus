93ad100f4ab2bfdd424ba15598db09c2d0400250
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-03-16 13:40:04 -0400

    add `never` to Sisyphus.ts

--- main/Sisyphus.ts
+++ main/Sisyphus.ts
@@ -1,19 +1,23 @@
 const NAME: "Sisyphus" = "Sisyphus"
 
-// should this be a classic `function`?
-const main = () => {
+// `function` is appropriate for subroutines,
+// but arrow/lambda is concise.
+const main: () => never = () => {
 	let chars: string = ""
 	// begin eternal torture
 	for (;;) {
 		// build while printing
 		while (chars.length < NAME.length) {
 			console.log(chars)
-      // `noUncheckedIndexedAccess` doesn't complain?
+			// `noUncheckedIndexedAccess` doesn't complain?
 			chars += NAME[chars.length]
 		}
 		// destroy all of our work, char by char
 		while (chars.length > 0) {
 			console.log(chars)
+			// `substring` is specific to `string`s,
+			// so we must use it for "safety",
+			// despite its verbosity.
 			chars = chars.substring(0, chars.length - 1)
 		}
 	}
