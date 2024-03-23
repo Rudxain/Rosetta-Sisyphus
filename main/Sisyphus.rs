df820bf00a0ee113264f3557883333fa1abbc267
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-03-23 14:02:57 -0400

    Create Sisyphus.rs
    simplified version

---
+++ main/Sisyphus.rs
@@ -0,0 +1,16 @@
+// no need for heap, only stack
+const NAME: &str = "Sisyphus";
+
+fn main() {
+	// begin eternal torture
+	for _ in 0..2 {
+		// give the illusion of building the string
+		for i in 0..=NAME.len() {
+			println!("{}", &NAME[0..i]);
+		}
+		// give the illusion of destroying chars
+		for i in (1..NAME.len()).rev() {
+			println!("{}", &NAME[0..i]);
+		}
+	}
+}
