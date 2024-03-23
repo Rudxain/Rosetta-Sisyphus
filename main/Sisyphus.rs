ab28c77cf18d062df854a6aa0c5da7382e965d36
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-03-23 14:06:22 -0400

    forgot `loop` again 💀

--- main/Sisyphus.rs
+++ main/Sisyphus.rs
@@ -3,7 +3,7 @@ const NAME: &str = "Sisyphus";
 
 fn main() {
 	// begin eternal torture
-	for _ in 0..2 {
+	loop {
 		// give the illusion of building the string
 		for i in 0..=NAME.len() {
 			println!("{}", &NAME[0..i]);
