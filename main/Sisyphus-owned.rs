6a984658281a50a7c4bdbc263f2b233fd50843fb
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2024-03-27 03:25:03 -0400

    refactor Sisyphus-owned.rs

--- main/Sisyphus-owned.rs
+++ main/Sisyphus-owned.rs
@@ -2,18 +2,20 @@ use core::str::from_utf8;
 // no need for heap, only stack
 const NAME: [u8; 8] = *b"Sisyphus";
 
+fn z<T: Iterator<Item = usize>>(iter: T) {
+    for i in iter {
+        // I wish there was a compile-time UTF8-validated
+        // fixed-size (stack-allocated) `copy`able ("small") string type.
+        println!("{}", from_utf8(&NAME[0..i]).unwrap());
+    }
+}
+
 fn main() {
-	// begin eternal torture
-	loop {
-		// give the illusion of building the string
-		for i in 0..=NAME.len() {
-			// I wish there was a compile-time UTF8-validated
-			// fixed-size (stack-allocated) `copy`able ("small") string type.
-			println!("{}", from_utf8(&NAME[0..i]).unwrap());
-		}
-		// give the illusion of destroying chars
-		for i in (1..NAME.len()).rev() {
-			println!("{}", from_utf8(&NAME[0..i]).unwrap());
-		}
-	}
+    // begin eternal torture
+    loop {
+        // give the illusion of building the string
+        z(0..=NAME.len());
+        // give the illusion of destroying chars
+        z((1..NAME.len()).rev());
+    }
 }
